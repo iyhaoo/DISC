0505f8b0795004fb7f3ca68f5c7cf3e80abf9dee
Hao Yuan <904469382@qq.com>
2020-01-25 17:38:19 +0800

    update

--- reproducibility/source/other_methods/revision/deepImpute.py
+++ reproducibility/source/other_methods/revision/deepImpute.py
@@ -1,71 +1,56 @@
-import loompy
-from deepimpute.multinet import MultiNet
+from deepimpute.deepImpute import deepImpute
 import numpy as np
 import h5py
 import pandas as pd
-import glob
 import argparse
 import os
 import time
+from collections import Counter
+
+
+def read_loom(loom_path):
+    assert os.path.exists(loom_path)
+    try:
+        with h5py.File(loom_path, "r", libver='latest', swmr=True) as f:
+            gene_name = f["row_attrs/Gene"][...].astype(np.str)
+            assert np.max(list(Counter(gene_name).values())) == 1, "{}".format(list(filter(lambda x: x[1] > 1, Counter(gene_name).items())))
+            gene_bc_mat = f["matrix"][...]
+            cell_id = f["col_attrs/CellID"][...].astype(np.str) if "col_attrs/CellID" in f.keys() else np.arange(gene_bc_mat.shape[1]).astype(np.str)
+        return gene_bc_mat, cell_id, gene_name
+    except OSError:
+        print("{}\nis connecting by other process, waiting...".format(loom_path))
+        time.sleep(5)
+        return read_loom(loom_path)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--filt-loom', required=True, type=str, help="Filtered data")
-parser.add_argument('--n-cores', required=False, type=int, default=1, help="Cores use")
+parser.add_argument('--loom', required=True, type=str, help="Loom")
+parser.add_argument('--n-cores', required=False, type=int, default=16, help="Number of cores.")
+parser.add_argument('--min-expressed-cell', required=False, type=int, default=10, help="min-expressed-cell")
+parser.add_argument("--min-expressed-cell-average-expression", required=False, type=float, default=1, help="min-expressed-cell-average-expression")
 FLAGS = vars(parser.parse_args())
-output_dir = "{}/imputation".format(FLAGS["filt_loom"].rsplit("/", 1)[0])
+output_dir = "{}/imputation".format(FLAGS["loom"].rsplit("/", 1)[0])
 os.makedirs(output_dir, exist_ok=True)
 
 starttime = time.time()
-with loompy.connect(FLAGS["filt_loom"]) as ds:
-    gene_bc_sparse = ds.sparse().astype(np.float32).tocsr()
-    gene_name = ds.ra["Gene"]
-    cell_id = ds.ca["CellID"]
-
-min_expressed_cell = 10
-min_expressed_cell_average_expression = 1
-expressed_cell = np.asarray((gene_bc_sparse > 0).sum(1)).squeeze()
-gene_expression = np.asarray(gene_bc_sparse.sum(1)).squeeze()
+gene_bc_mat, cell_id, gene_name = read_loom(FLAGS["loom"])
+min_expressed_cell = FLAGS["min_expressed_cell"]
+min_expressed_cell_average_expression = FLAGS["min_expressed_cell_average_expression"]
+expressed_cell = (gene_bc_mat > 0).sum(1)
+gene_expression = gene_bc_mat.sum(1)
 gene_filter = np.bitwise_and(expressed_cell >= min_expressed_cell, gene_expression > expressed_cell * min_expressed_cell_average_expression)
-input_gene_bc_mat = gene_bc_sparse[gene_filter, :]
+input_gene_bc_mat = gene_bc_mat[gene_filter, :]
 print(input_gene_bc_mat.shape)
-data = pd.DataFrame(input_gene_bc_mat.toarray()).T
-multinet = MultiNet(n_cores=FLAGS["n_cores"])
-multinet.fit(data, cell_subset=1)
-imputedData = multinet.predict(data)
+# dimension = (cells x genes)
+data = pd.DataFrame(input_gene_bc_mat.toarray(), index=gene_name[gene_filter], columns=cell_id).T
+print(data)
+imputed = deepImpute(data, NN_lim='auto', n_cores=FLAGS["n_cores"], cell_subset=1)
+print(imputed)
 input_loom_name = FLAGS["filt_loom"].rsplit("/", 1)[1]
 output_h5 = input_loom_name.replace(".loom", "_deepImpute_mc_{}_mce_{}.hdf5".format(min_expressed_cell, min_expressed_cell_average_expression))
 with h5py.File("{}/{}".format(output_dir, output_h5), "w") as f:
     f["cell_id"] = cell_id.astype(h5py.special_dtype(vlen=str))
     f["gene_name"] = gene_name[gene_filter].astype(h5py.special_dtype(vlen=str))
     if_dset_imputation = f.create_dataset("imputation", shape=(cell_id.size, gene_filter.sum()), chunks=(1, gene_filter.sum()), dtype=np.float32)
-    if_dset_imputation[...] = imputedData.values
+    if_dset_imputation[...] = imputed.values
 print(pd.to_timedelta(time.time() - starttime, unit="s"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
