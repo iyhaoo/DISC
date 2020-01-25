59ec14ea8941a09d2cb64f2ca92189e0f5c25b69
Hao Yuan <904469382@qq.com>
2020-01-25 23:22:36 +0800

    Update VIPER.r

--- reproducibility/source/other_methods/revision/VIPER.r
+++ reproducibility/source/other_methods/revision/VIPER.r
@@ -1,20 +1,9 @@
 args<-commandArgs(trailingOnly=TRUE)
-if(length(args) != 1){
-  stop("R --slave < this_code.r --args <filt-loom>")
+if(length(args) != 2){
+  stop("R --slave < this_code.r --args <loom> <cell/gene>")
 }
-#install.packages("devtools")
-#library(devtools)
-#install_github("ChenMengjie/VIPER")
+source("/home/yuanhao/single_cell/scripts/evaluation_pipeline/evaluation/utilities.r")
 library(VIPER)
-library(loomR)
-library(rhdf5)
-
-delete_last_element <- function(x){
-  return(x[1: (length(x) - 1)])
-}
-get_last_element <- function(x){
-  return(x[length(x)])
-}
 
 starttime = Sys.time()
 loom_path = args[1]
@@ -32,28 +21,39 @@ if(file.exists(output_h5)){
   print("pass")
   next()
 }
-raw_data_loompy = connect(loom_path)
-gene_bc_mat = t(as.matrix(raw_data_loompy$matrix[, ]))
-gene_name = raw_data_loompy[["row_attrs/Gene"]][]
-cell_id = raw_data_loompy[["col_attrs/CellID"]][]
-raw_data_loompy$close_all()
-expressed_cell = Matrix::rowSums(gene_bc_mat > 0)
-gene_expression = Matrix::rowSums(gene_bc_mat)
+gene_bc_mat = readh5_loom(loom_path)
+gene_name = rownames(gene_bc_mat)
+cell_id = colnames(gene_bc_mat)
+expressed_cell = rowSums(gene_bc_mat > 0)
+gene_expression = rowSums(gene_bc_mat)
 gene_filter = expressed_cell >= min_expressed_cell & gene_expression > expressed_cell * min_expressed_cell_average_expression
 filt_data = gene_bc_mat[gene_filter, ]
+print(dim(filt_data))
+print(filt_data[1:5, 1:5])
 gene_name_filt = gene_name[gene_filter]
 rownames(filt_data) = paste0("Gene_", 1:length(gene_name_filt))
 colnames(filt_data) = paste0("Cell_", 1:length(cell_id))
 out_dir = paste(method_dir, sub(".loom", paste0("_VIPER_mc_", min_expressed_cell, "_mce_", min_expressed_cell_average_expression), get_last_element(unlist(strsplit(loom_path, "/", fixed = T))), fixed = TRUE), sep = "/")
 dir.create(out_dir, showWarnings = F, recursive = T)
-print(filt_data[1:5, 1:5])
-print(dim(filt_data))
-res <- VIPER(gene.expression = filt_data, outdir = out_dir)
-row.names(res$imputed_log) = gene_name_filt
-row.names(res$imputed) = gene_name_filt
-colnames(res$imputed_log) = cell_id
-colnames(res$imputed) = cell_id
 output_rds_name = sub(".loom", paste0("_VIPER_mc_", min_expressed_cell, "_mce_", min_expressed_cell_average_expression, ".rds"), get_last_element(unlist(strsplit(loom_path, "/", fixed = T))), fixed = TRUE)
+
+if(args[2] == "cell"){
+  res <- VIPER(gene.expression, num = 5000, percentage.cutoff = 0.1, minbool = FALSE, alpha = 1, report = FALSE, outdir = out_dir, prefix = NULL)
+  rownames(res$imputed_log) = gene_name_filt
+  rownames(res$imputed) = gene_name_filt
+  colnames(res$imputed_log) = cell_id
+  colnames(res$imputed) = cell_id
+  imputed_mat = res$imputed
+}elif(args[2] == "gene"){
+  res = VIPER(t(filt_data), num = 1000, percentage.cutoff = 0.5, minbool = FALSE, alpha = 0.5, report = FALSE, outdir = out_dir, prefix = NULL)
+  colnames(res$imputed_log) = gene_name_filt
+  colnames(res$imputed) = gene_name_filt
+  rownames(res$imputed_log) = cell_id
+  rownames(res$imputed) = cell_id
+  imputed_mat = t(res$imputed)
+}else{
+  stop("args[2] must be cell or gene")
+}
 saveRDS(res, paste(method_dir, output_rds_name, sep = "/"))
 h5createFile(output_h5)
 h5createDataset(file = output_h5,
@@ -63,6 +63,6 @@ h5createDataset(file = output_h5,
                 chunk=c(length(gene_name_filt), 1))
 h5write(cell_id, output_h5,"cell_id")
 h5write(gene_name_filt, output_h5,"gene_name")
-h5write(res$imputed, file=output_h5, name="imputation")
+h5write(imputed_mat, file=output_h5, name="imputation")
 print(Sys.time() - starttime)
 
