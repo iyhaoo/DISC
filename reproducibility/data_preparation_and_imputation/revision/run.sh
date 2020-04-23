1ed48b88e8b8668edf1954015e921764e1a0b3d3
iyhaoo <tyhaoo@gmail.com>
2020-04-23 11:28:52 +0800

    update

--- reproducibility/data_preparation_and_imputation/revision/run.sh
+++ reproducibility/data_preparation_and_imputation/revision/run.sh
@@ -112,3 +112,70 @@ python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_meth
 --loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT_293T/raw.loom \
 --min-expressed-cell=10
 
+
+###   0.51
+disc \
+--dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--out-dir=/home/yuanhao/DISC_imputation_result/JURKAT_0.51 \
+--min-expressed-cell=10 \
+--library-size-factor=median
+
+
+disc \
+--dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/293T/raw.loom \
+--out-dir=/home/yuanhao/DISC_imputation_result/293T_0.51 \
+--min-expressed-cell=10 \
+--library-size-factor=median
+
+
+CUDA_VISIBLE_DEVICES=5 HDF5_USE_FILE_LOCKING=FALSE disc \
+--dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT_293T/raw.loom \
+--out-dir=/home/yuanhao/DISC_imputation_result/JURKAT_293T_0.51 \
+--min-expressed-cell=10 \
+--library-size-factor=median
+##########
+
+
+disc \
+--dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
+--out-dir=/home/yuanhao/DISC_imputation_result/10X_5CL \
+--min-expressed-cell=10 \
+--library-size-factor=median
+
+
+R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/VIPER.r \
+--args /home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom gene 10
+
+
+R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scImpute.r \
+--args /home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom 16 10
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/MAGIC.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--min-expressed-cell=10
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/deepImpute.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--min-expressed-cell=10
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/DCA.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--min-expressed-cell=10
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scVI.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--min-expressed-cell=10
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scScope.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--min-expressed-cell=10
+
+
+
+
+
