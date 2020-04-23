eb9b1c4101b8daeab1c7cd02f55daae98f94f0e9
iyhaoo <tyhaoo@gmail.com>
2020-04-23 22:29:32 +0800

    Update run.sh

--- reproducibility/data_preparation_and_imputation/revision/run.sh
+++ reproducibility/data_preparation_and_imputation/revision/run.sh
@@ -136,7 +136,7 @@ CUDA_VISIBLE_DEVICES=5 HDF5_USE_FILE_LOCKING=FALSE disc \
 ##########
 
 
-disc \
+CUDA_VISIBLE_DEVICES=5 HDF5_USE_FILE_LOCKING=FALSE disc \
 --dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --out-dir=/home/yuanhao/DISC_imputation_result/10X_5CL \
 --min-expressed-cell=10 \
@@ -144,38 +144,78 @@ disc \
 
 
 R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/VIPER.r \
---args /home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom gene 10
+--args /home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom gene 10
 
 
 R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scImpute.r \
---args /home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom 16 10
+--args /home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom 16 10
 
 
 python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/MAGIC.py \
---loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --min-expressed-cell=10
 
 
 python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/deepImpute.py \
---loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --min-expressed-cell=10
 
 
 python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/DCA.py \
---loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --min-expressed-cell=10
 
 
 python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scVI.py \
---loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --min-expressed-cell=10
 
 
 python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scScope.py \
---loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/JURKAT/raw.loom \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/10X_5CL/raw.loom \
 --min-expressed-cell=10
 
 
 
+
+
+CUDA_VISIBLE_DEVICES=5 HDF5_USE_FILE_LOCKING=FALSE disc \
+--dataset=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--out-dir=/home/yuanhao/DISC_imputation_result/PBMC_TISSUE \
+--min-expressed-cell=60 \
+--library-size-factor=median
+
+
+R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/VIPER.r \
+--args /home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom gene 60
+
+
+#R --slave < /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scImpute.r \
+#--args /home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom 16 60
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/MAGIC.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--min-expressed-cell=60
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/deepImpute.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--min-expressed-cell=60
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/DCA.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--min-expressed-cell=60
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scVI.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--min-expressed-cell=60
+
+
+python3 /home/yuanhao/github_repositories/DISC/reproducibility/source/other_methods/scScope.py \
+--loom=/home/yuanhao/github_repositories/DISC/reproducibility/data/PBMC_TISSUE/raw.loom \
+--min-expressed-cell=60
 
 
